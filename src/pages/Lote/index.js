90a20f4a8e27a3407ca203b664e34271dfca0fce
Daniel Kitanaxi Filipe <danielkitanaxifilipe12@gmail.com>
2022-11-15 22:03:58 -0300

    lote
    lote

--- src/pages/Lote/index.js
+++ src/pages/Lote/index.js
@@ -14,6 +14,7 @@ import {
   HiRefresh,
   HiOutlineSearch,
   HiOutlineEye,
+  HiOutlinePlusSm,
   HiOutlineUserCircle
 } from "react-icons/hi";
 import {
@@ -27,19 +28,16 @@ import {
   IoPersonOutline
 } from "react-icons/io5";
 import { MenuTop } from '../../components/MenuTop';
-import { Modal, Button, } from 'react-bootstrap';
+import { Modal, Button, Spinner } from 'react-bootstrap';
 
 import {
   Card,
   Table,
   Dropdown,
   Collapse,
-  Form,
-  Spinner
+  Form
 } from 'react-bootstrap';
-
-import { api } from './../../services/api';
-
+import { api } from '../../services/api';
 import marisa from './../../assets/photos/marisa.jpg'
 import chiao from './../../assets/photos/chiao.jpg'
 import cassia from './../../assets/photos/cassia.jpg'
@@ -47,22 +45,30 @@ import matheus from './../../assets/photos/matheus.jpg'
 import paula from './../../assets/photos/paula.jpg'
 
 
-function Quadra() {
+function Lote() {
   const [open1, setOpen1] = useState(false);
 
-  const [blocks, setBlocks] = useState([]);
+  const [typologies, setTypologies] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [loadingSubmitTypology, setLoadingSubmitTypology] = useState(false);
+
+  const [typology, setTypology] = useState("");
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     setLoading(true);
-    getBlocks();
+    getTypologies();
 
   }, []);
 
-  async function getBlocks() {
+  async function getTypologies() {
     try {
-      const response = await api.get('v1/listCategoria/1');
-      setBlocks(response.data);
+      const response = await api.get('v1/listTipologias/1');
+      setTypologies(response.data);
 
       setLoading(false);
     } catch (error) {
@@ -83,7 +89,7 @@ function Quadra() {
     try {
       setLoading(true);
       const response = await api.get(link);
-      setBlocks(response.data);
+      setTypologies(response.data);
 
       setLoading(false);
     } catch (error) {
@@ -104,7 +110,7 @@ function Quadra() {
     try {
       setLoading(true);
       const response = await api.get(link);
-      setBlocks(response.data);
+      setTypologies(response.data);
 
       setLoading(false);
     } catch (error) {
@@ -121,6 +127,39 @@ function Quadra() {
     }
   }
 
+  async function handleSaveTypology(condominio_id) {
+    try {
+      setLoadingSubmitTypology(true);
+
+      if (!typology) return alert('Tipologia é obrigatório!');
+
+      const data = {
+        tipologia: typology,
+        condominio_id: condominio_id
+      }
+
+      const response = await api.post('v1/addTipologia', data);
+
+      if (response.data.success) {
+        alert(response.data.msg);
+      }
+
+      handleClose();
+      getTypologies();
+      setLoadingSubmitTypology(false);
+    } catch (error) {
+      if (error.message === "Network Error") {
+        console.log("Por favor verifique sua conexão com a internet!");
+      } else if (error.message === "Request failed with status code 401") {
+        console.log("Erro ao carregar cursos, por favor, tente recarregar a página!");
+      } else if (error.message === "Request failed with status code 400") {
+        console.log("Erro ao carregar cursos, por favor, tente recarregar a página!");
+      } else if (error.status === 500) {
+        console.log("Erro interno, por favor, contactar o suporte!");
+      }
+      setLoadingSubmitTypology(false);
+    }
+  }
   return (
     <div className="dashboard">
       <main className='d-flex'>
@@ -129,7 +168,7 @@ function Quadra() {
           <MenuTop />
           <div className="p-4">
             <div className="container">
-              <div className='d-flex w-max-1200 justify-content-between'>
+              <div className='d-flex justify-content-between w-max-1200'>
                 <div><h4 className=''>Estrutura</h4></div>
                 <div>
                   <Button
@@ -139,9 +178,12 @@ function Quadra() {
                   >
                     <HiAdjustments />
                   </Button>
-
                   <Button className='btn-sm ms-1'>
                     <HiRefresh />
+                  </Button>
+
+                  <Button className='btn-sm ms-1' onClick={handleShow}>
+                    <HiOutlinePlusSm />
                   </Button>
                 </div>
               </div>
@@ -166,8 +208,8 @@ function Quadra() {
               <div className='mt-4'>
                 <div className="btn-group border-botton-right-0">
                   <Link to="/typology" className="btn border-botton-right-0 btn-light-tabs " >Tipologias</Link>
-                  <Link to="/lote" className="btn border-botton-right-0 btn-light-tabs ">Lote</Link>
-                  <Link to="/quadra" className="btn border-botton-right-0 btn-light-tabs active">Quadra</Link>
+                  <Link to="/lote" className="btn border-botton-right-0 btn-light-tabs active">Lote</Link>
+                  <Link to="/quadra" className="btn border-botton-right-0 btn-light-tabs ">Quadra</Link>
                 </div>
                 <div className="card border-0 border-botton-right-left-0 card-table">
                   <div className="card-body pb-2"></div>
@@ -175,18 +217,31 @@ function Quadra() {
                     <table className="table">
                       <thead>
                         <tr>
-                          <th className='ps-4'>Identificador</th>
-                          <th>Nº de Residência</th>
+                          <th className='ps-4'>Tipo</th>
+                          <th>Nº Quadra</th>
+                          <th>Nº Residência</th>
+                          <th>Nº Estabelecimento</th>
                           <th className='text-right pe-4'>Acções</th>
                         </tr>
                       </thead>
                       <tbody>
+
                         {
                           !loading ?
-                            blocks?.data?.map(block => (
-                              <tr key={block.id}>
-                                <th scope="row" className='ps-4'>{block.quadra}</th>
-                                <th>{block.total_residencia}</th>
+                            typologies?.data?.map(employee => (
+                              <tr key={employee.id}>
+                                <th scope="row" className='ps-4'>
+                                  {employee.name}
+                                </th>
+                                <th>
+                                  {employee.name}
+                                </th>
+                                <th>
+                                  {employee.name}
+                                </th>
+                                <th>
+                                  {employee.name}
+                                </th>
                                 <td className='text-right pe-4'>
                                   <Dropdown>
                                     <Dropdown.Toggle className="btn btn-light p-0 m-0 " id="dropdown-basic">
@@ -231,7 +286,7 @@ function Quadra() {
                         {
                           !loading ?
 
-                            blocks?.from + ' - ' + blocks?.to + '- ' + blocks?.total : '0 - 0 itens '
+                            typologies?.from + ' - ' + typologies?.to + '- ' + typologies?.total : '0 - 0 itens '
 
                         }
                         itens
@@ -242,8 +297,8 @@ function Quadra() {
                             {
                               !loading ?
 
-                                blocks?.prev_page_url ?
-                                  <li className="page-item"><button className="page-link" onClick={() => handlePrevPage(blocks?.prev_page_url)} href="#">&laquo;</button></li>
+                                typologies?.prev_page_url ?
+                                  <li className="page-item"><button className="page-link" onClick={() => handlePrevPage(typologies?.prev_page_url)} href="#">&laquo;</button></li>
                                   : <li className="page-item"><button className="page-link">&laquo;</button></li>
                                 :
                                 <>
@@ -252,8 +307,8 @@ function Quadra() {
                             }
 
                             {
-                              blocks?.next_page_url ?
-                                <li className="page-item"><button className="page-link" onClick={() => handleNextPage(blocks?.next_page_url)}>&raquo;</button></li>
+                              typologies?.next_page_url ?
+                                <li className="page-item"><button className="page-link" onClick={() => handleNextPage(typologies?.next_page_url)}>&raquo;</button></li>
                                 : <li className="page-item"><button className="page-link" >&raquo;</button></li>
                             }
                           </ul>
@@ -267,8 +322,47 @@ function Quadra() {
           </div>
         </section>
       </main>
+
+      {/*modal*/}
+      <Modal show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}>
+        <Modal.Header closeButton className='border-0'>
+          <h5 className='mt-3'>Adicionar tipologia</h5>
+        </Modal.Header>
+        <Modal.Body className='pt-0'>
+          <form action="">
+            <label className='mt-2 mb-2'><b>Tipologia *</b></label>
+            <Form.Control
+              type="text"
+              required
+              placeholder="Tipologia"
+              onChange={(event) => setTypology(event.target.value)}
+            />
+          </form>
+        </Modal.Body>
+        <Modal.Footer className='border-0'>
+          <Button variant="secondary" onClick={handleClose} className='btn-sm'>
+            Cancelar
+          </Button>
+
+          {
+
+            !loadingSubmitTypology
+              ? <Button variant="primary" onClick={() => handleSaveTypology(1)} className='btn-sm'>
+                Adicionar
+              </Button>
+              : <Button variant="primary" disabled className='btn-sm'>
+                Adicionando...
+              </Button>
+
+          }
+        </Modal.Footer>
+      </Modal>
+      {/*modal*/}
     </div>
   );
 }
 
-export default Quadra;
+export default Lote;
